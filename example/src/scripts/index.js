3c201c35736b7326b507f081dc29f8a2d6f45ec6
xulihang <xulihanghai@163.com>
2024-01-03 15:44:58 +0800

    do not set layout

--- example/src/scripts/index.js
+++ example/src/scripts/index.js
@@ -64,8 +64,8 @@ async function initialize(){
   loadResolutions();
   startBtn.innerText = "Start Camera";
   startBtn.disabled = "";
-  console.log("set layout");
-  await CameraPreview.setLayout({left:"80%",width:"20%",top:"0%",height:"20%"});
+  //console.log("set layout");
+  //await CameraPreview.setLayout({left:"80%",width:"20%",top:"0%",height:"20%"});
 }
 
 async function updateViewfinder(res){
